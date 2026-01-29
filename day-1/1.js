6eceade920c36c7ebe2bf8c2b7ffe2fff606d081
Rakib Dhali <rakibdhali343@gmail.com>
2026-01-29 12:02:58 +0600

    commit 02

---
+++ day-1/1.js
@@ -0,0 +1,7 @@
+// print number from 1 to N
+
+let N = 10;
+
+for (let i = 1; i <= N; i++) {
+  console.log(i);
+}
--- day-1/2.js
+++ day-1/2.js
@@ -1,15 +1,7 @@
-// print number from 1 to N
-
-let N = 10;
-
-for (let i = 1; i <= N; i++) {
-  console.log(i);
-}
-
 // Print Numbers from N to 1 without changing the loop condition of above question
 
 let N = 10;
 
 for (let i = 1; i <= N; i++) {
-  console.log(i);
+  console.log(N - i + 1);
 }
