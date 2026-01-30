6eb601ed258ca6889946f054a6e6887b20436df6
Rakib Dhali <rakibdhali343@gmail.com>
2026-01-30 23:06:42 +0600

    commit 05

---
+++ day-2/1.js
@@ -0,0 +1,14 @@
+// Find the Maximum of Three Numbers
+let a = 3;
+let b = 3;
+let c = 3;
+
+if (a > b && a > c) {
+  console.log(a, " is the highest number among three numbers");
+} else if (b > a && b > c) {
+  console.log(b, " is the highest number among three numbers");
+} else if (c > a && c > b) {
+  console.log(c, " is the highest number among three numbers");
+} else {
+  console.log("seem some numbers are equal");
+}
---
+++ day-2/2.js
@@ -0,0 +1,9 @@
+// Check if a Number is Positive, Negative, or Zero
+
+let input = 8;
+
+if (input === 0) {
+    console.log('the input number is Zero',);
+}else if (input > 0){console.log('the input number is positive');}
+else if (input < 0){ console.log('the input number is negative');}
+
