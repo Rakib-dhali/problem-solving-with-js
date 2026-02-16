39bd5033b2574a73d7428f53f9c2379eec5282f3
Rakib Dhali <rakibdhali343@gmail.com>
2026-02-16 13:25:09 +0600

    commit 11

--- day-3/h3.js
+++ day-3/h3.js
@@ -14,21 +14,26 @@
 //      * *
 //       *
 
-let rows = 5;
+let rows = 3;
+let maxRow = 2 * rows - 1;
 
-for (let i = 1; i <= 2 * rows - 1; i++) {
+for (let i = 1; i <= maxRow; i++) {
   let stars, fSpace, mSpaces;
-  if (i === 1 || i === 2 * rows - 1) {}
-    stars = 1;
-    if (i <= rows) {
-      for (let j = rows; j >= 1; j--){ 
-        fSpace = j;
-        mSpaces = 
-        console.log(" ".repeat(fSpace) + "*".repeat(stars));}
-    }else{
-        for (let j = rows+1; j >= 2*rows-1 ; j++){ 
-        fSpace = j;
-        console.log(" ".repeat(fSpace) + "*".repeat(stars));}
+  if (i === 1 || i === 2 * rows - 1) {
+    i === 1 ? (fSpace = rows - i) : (fSpace = i - rows);
+    console.log(" ".repeat(fSpace) + "*");
+  } else {
+    if (i < rows) {
+      fSpace = rows - i;
+      mSpaces = maxRow - stars - 2 * fSpace;
+      console.log(" ".repeat(fSpace) + "*" + " ".repeat(mSpaces) + "*");
+    } else if (i > rows) {
+      fSpace = i - rows;
+      mSpaces = maxRow - stars - 2 * fSpace;
+      console.log(" ".repeat(fSpace) + "*" + " ".repeat(mSpaces) + "*");
+    } else {
+      mSpaces = maxRow - stars;
+      console.log("*" + " ".repeat(mSpaces) + "*");
     }
   }
-
+}
